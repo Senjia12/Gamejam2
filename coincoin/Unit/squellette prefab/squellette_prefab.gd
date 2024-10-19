866ab95caf22fcae4b6b28baf3ed41f65821cbbf
Loumunix <olivier@bru-barth.com>
2024-10-19 13:30:15 +0200

    squelette prefab finit

--- coincoin/Unit/squellette prefab/squellette_prefab.gd
+++ coincoin/Unit/squellette prefab/squellette_prefab.gd
@@ -2,6 +2,10 @@ extends CharacterBody2D
 
 @export var speed := 5000
 @export var range := 1.0
+@export var attack_damage := 5
+@export var max_hp := 25
+var current_hp := 25
+
 
 @onready var nav = get_node("NavigationAgent2D")
 var target_position = null
@@ -11,6 +15,12 @@ var is_a_moving := false
 var is_selected := false
 
 var humain_in_range := false
+
+
+func _ready() -> void:
+	$Attack_range/CollisionShape2D.scale = Vector2(range,range)
+	current_hp = max_hp
+
 
 func _physics_process(delta: float) -> void:
 	if is_moving == true or is_a_moving && !humain_in_range:
@@ -33,6 +43,23 @@ func move_along_path(delta):
 		move_and_slide()
 
 
+func take_damage(dmg):
+	current_hp -= dmg
+	if current_hp <= 0:
+		queue_free()
+
+
 func _on_attack_range_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Humain"):
-		humain_in_range = true
+		if !humain_in_range:
+			humain_in_range = true
+			$"attack cd".start()
+
+func _on_attack_range_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Humain"):
+		if $Attack_range.get_overlapping_bodies() == []:
+			humain_in_range = false
+			$"attack cd".stop()
+
+func _on_attack_cd_timeout() -> void:
+	$Attack_range.get_overlapping_bodies()[0].take_damage(attack_damage)
