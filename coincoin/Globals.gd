26e5cd347cdd38618f97ae979bf5a0ce865561ca
Loumunix <olivier@bru-barth.com>
2024-10-19 15:36:33 +0200

    fix navmesh + herbe

--- coincoin/Globals.gd
+++ coincoin/Globals.gd
@@ -1,7 +1,7 @@
 extends Node
 
 
-var night := true
+var night := false
 
 
 
--- coincoin/Unit/nécromancien.gd
+++ coincoin/Unit/nécromancien.gd
@@ -1,11 +1,6 @@
 extends CharacterBody2D
 
-
-func _ready() -> void:
-	Globals.player = self
-	
 #variables stats
-@onready var shadow_area: Area2D = $Nécromancien
 var speed = 100
 var cooldown_multiplayer = 1.0
 var regen = 1.0
@@ -13,9 +8,14 @@ var pv_max = 100
 var pv = 100
 var nb_ames = 10
 
+
+func _ready() -> void:
+	Globals.player = self
+
+
 func _physics_process(delta):
-	var idle_player = $"idle player"
 	velocity.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	velocity.y = Input.get_action_strength("bottom") - Input.get_action_strength("top")
+	velocity.y = Input.get_action_strength("down") - Input.get_action_strength("up")
 	velocity = velocity.normalized()
 	velocity *= speed
+	move_and_slide()
