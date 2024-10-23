790bbb74afbad4d2728d68740e5ea17546047318
Senjia12 <senjiacorp@gmail.com>
2024-10-23 17:48:45 +0200

    necro anims move et fin summon spell

--- coincoin/Unit/nécromancien/nécromancien.gd
+++ coincoin/Unit/nécromancien/nécromancien.gd
@@ -7,11 +7,11 @@ var regen = 1.0
 var pv_max = 100
 var pv = 100
 var nb_ames = 10
-@onready var spawn_node: NavigationRegion2D = $"../Terrain/NavigationRegion2D"
-
+var player_direction = "idle_forward"
 
 func _ready() -> void:
 	Globals.player = self
+	$necro.play("idle_forward")
 
 
 func _physics_process(delta):
@@ -20,3 +20,46 @@ func _physics_process(delta):
 	velocity = velocity.normalized()
 	velocity *= speed
 	move_and_slide()
+	
+	if velocity != Vector2.ZERO :
+	
+		if velocity.x > 0:
+			$necro.flip_h = false
+			$necro.play("walk_to_the_right")
+			player_direction = "right"
+	
+		elif velocity.x < 0:
+			$necro.flip_h = true
+			$necro.play("walk_to_the_right")
+			player_direction = "left"
+	
+		if velocity.y > 0:
+			$necro.play("walk_forward")
+			player_direction = "front"
+	
+		elif velocity.y < 0:
+			$necro.play("walk_backward")
+			player_direction = "back"
+	
+	else:
+		
+		if player_direction == "right":
+			$necro.flip_h = false
+			$necro.play("idle_right")
+
+		if player_direction == "left":
+			$necro.flip_h = true
+			$necro.play("idle_right")
+
+		if player_direction == "front":
+			$necro.play("idle_forward")
+
+		if player_direction == "back":
+			$necro.play("idle_backward")
+
+
+		
+		
+		
+		
+		
