c96fcccb29879a37ece4be8e3d8ad8d994c7e969
Loumunix <olivier@bru-barth.com>
2024-10-26 22:22:59 +0200

    poti patch

--- coincoin/Level/intro.gd
+++ coincoin/Level/intro.gd
@@ -6,7 +6,15 @@ func  _ready() -> void:
 	if Globals.first_game:
 		$ColorRect.show()
 		$TextureRect.show()
-		play("intro")
 		Globals.first_game = false
 	else:
 		queue_free()
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		play("intro")
+
+
+func _on_animation_finished(anim_name: StringName) -> void:
+	queue_free()
